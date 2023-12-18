bdb9e46132c4e2fbc5e852d9611f71e388d0f987
Bartosz Spiżarny <64313992+Bartolomeo26@users.noreply.github.com>
2023-12-18 18:01:01 +0100

    Dodane pliki

---
+++ Zadanie6-ios/Zadanie6-ios/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Zadanie6-ios
+//
+//  Created by Bartosz Spizarny on 17/12/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Zadanie6-ios/Zadanie6-ios/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Zadanie6-ios
+//
+//  Created by Bartosz Spizarny on 17/12/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
